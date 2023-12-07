8c159db4bb37c6f8dddc36385bff2ee6365eb968
ShivamAgrawal150199 <shivam.agrawal1501@gmail.com>
2023-12-07 20:49:56 +0530

    video 16 completed - react router dom

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -6,6 +6,9 @@ import './App.css';
 import TextForm from './components/TextForm';
 import { useState } from 'react';
 import Alert from './components/Alert';
+import { BrowserRouter as Router, Switch , Link, Route, Routes} from "react-router-dom";
+import About from './components/About';
+
 
 // let name="Shivam";
 function App() {
@@ -28,12 +31,20 @@ const toggleMode=()=>{
     setMode('light');
     document.body.style.backgroundColor = 'white';
     showAlert("Light mode has been enabled","success");
+    document.title='TextUtils - Light Mode';
   }
   else
   {
     setMode('dark');
     document.body.style.backgroundColor = '#042743';
     showAlert("Dark mode has been enabled","success");
+    document.title='TextUtils - Dark Mode';
+    setInterval(() => {
+      document.title='TextUtils is an amazing app';
+    }, 2000);
+    setInterval(() => {
+      document.title='Install TextUtils now';
+    }, 1500);
   }
   
 }
@@ -42,14 +53,26 @@ const toggleMode=()=>{
     // <><Navbar title="Text Utiles new" abouttext="about"></Navbar></>
     
     <>
+    <Router>
     <Navbar title='Text Utils' abouttext='About us' mode={mode} toggleMode={toggleMode}></Navbar>
     <Alert alert={alert}/>
     <div className="container my-3">
       {/* <About/> */}
-      <TextForm heading='Convert the text' mode={mode} showAlert={showAlert}/>
+      
+      <Routes>
+        <Route exact path="/About" element={<About/>}>
+          
+        </Route>
+        <Route exact path="/" element={<TextForm heading='Convert the text' mode={mode} showAlert={showAlert}/>}>
+           
+        </Route>
+      </Routes>
+      </div>
+      </Router>
     {/* <TextForm heading="Enter the text to analyze below"></TextForm> */}
+    {/* <About></About> */}
 
-    </div>
+    
     
     </>
 
--- reactapp/src/components/Navbar.js
+++ reactapp/src/components/Navbar.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 
 
 export default function Navbar(props) {
   return (
     
       <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
-  <a className="navbar-brand" href="/">{props.title}</a>
+  <Link className="navbar-brand" to="/">{props.title}</Link>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
@@ -14,10 +15,10 @@ export default function Navbar(props) {
   <div className="collapse navbar-collapse" id="navbarSupportedContent">
     <ul className="navbar-nav mr-auto">
       <li className="nav-item active">
-        <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
+        <Link className="nav-link" to="/">Home <span className="sr-only">(current)</span></Link>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="/">{props.abouttext}</a>
+        <Link className="nav-link" to="/About">{props.abouttext}</Link>
       </li>
       <li className="nav-item dropdown">
         <a className="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
