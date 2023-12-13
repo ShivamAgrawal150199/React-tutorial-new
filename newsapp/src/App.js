23aea09d1f87593c97e7dc4fc56663ba6efb3134
ShivamAgrawal150199 <shivam.agrawal1501@gmail.com>
2023-12-14 01:12:44 +0530

    added news categories in navbar - video 31 completed

--- newsapp/src/App.js
+++ newsapp/src/App.js
@@ -4,15 +4,41 @@ import './App.css';
 import React, { Component } from 'react'
 import Navbar from './component/Navbar';
 import News from './component/News';
+import {
+  BrowserRouter as Router,
+  Route,
+ Routes
+} from "react-router-dom";
 
 export default class App extends Component {
   c='john';
   render() {
     return (
       <div>
-        <Navbar></Navbar>
-        <News pageSize={5} country='in' category='sports'></News>
-        {/* Hello first class based component {this.c} */}
+
+
+        <Router>
+        <Navbar/>
+
+          <Routes>
+
+
+            <Route exact path='/' element={<News key='general' category='general'></News>}></Route>
+            <Route exact path='/business' element={<News key='business' category='business'></News>}></Route>
+            <Route exact path='/entertainment' element={<News key='entertainment' category='entertainment'></News>}></Route>
+            <Route exact path='/health' element={<News key='health' category='health'></News>}></Route>
+            <Route exact path='/science' element={<News key='science' category='science'></News>}></Route>
+            <Route exact path='/sports' element={<News key='sports' category='sports'></News>}></Route>
+            <Route exact path='/technology' element={<News key='technology' category='technology'></News>}></Route>
+            
+
+        
+        
+          
+          
+          {/* Hello first class based component {this.c} */}
+        </Routes>
+        </Router>
       </div>
     )
   }
