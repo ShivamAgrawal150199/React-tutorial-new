a2bb92c22074327c3434a27ec08abbba6bb2bbe9
ShivamAgrawal150199 <shivam.agrawal1501@gmail.com>
2023-12-17 13:46:11 +0530

    connected mongodb server - video 43 completed

---
+++ inotebook/backend/db.js
@@ -0,0 +1,22 @@
+const mongoose=require('mongoose');
+const mongoURI="mongodb://127.0.0.1:27017/"
+
+// const connecttoMongo =()=>{
+//     mongoose.connect(mongoURI,()=>{
+//         console.log('mongo connected successfully');
+//     })
+// }
+
+const connecttoMongo =  async () => {
+    console.log("here")
+    // try {
+    //    await mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+    //   console.log("Connected to MongoDB successfully");
+    // } catch (error) {
+    //   console.error("Error connecting to MongoDB:", error);
+    // }
+
+    mongoose.connect(mongoURI).then(()=>console.log("Connected")).catch((e)=>console.log("error "+e.message))
+  };
+
+module.exports=connecttoMongo;
