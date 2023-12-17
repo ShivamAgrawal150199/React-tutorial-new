e7c357bbb98f0e02d23b9316ae9a80f508f1ac05
ShivamAgrawal150199 <shivam.agrawal1501@gmail.com>
2023-12-17 16:23:51 +0530

    created 2 schemas and 2 end point api - video 44 completed

--- inotebook/backend/index.js
+++ inotebook/backend/index.js
@@ -10,6 +10,11 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+//available routes
+
+app.use('/api/auth',require('./routes/auth'))
+app.use('/api/notes',require('./routes/notes'))
+
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
 })
