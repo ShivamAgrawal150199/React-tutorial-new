b7635e6eb740d363e9e5c1073be7dbb55fbe024a
ShivamAgrawal150199 <shivam.agrawal1501@gmail.com>
2023-12-17 20:44:11 +0530

    created validation for data insertion, added unique row feature. video 46 completed

--- inotebook/backend/routes/auth.js
+++ inotebook/backend/routes/auth.js
@@ -1,19 +1,36 @@
 const express=require('express');
 const User = require('../models/User');
 const router= express.Router();
+const { query, validationResult, body } = require('express-validator');
 
-router.post('/',(req,res)=>{
+
+router.post('/',[
+    body('email','Enter a valid email').isEmail(),
+    body('name','Enter a valid name').isLength({min:3}),
+    body('password','Enter a valid password').isLength({min:5})
+    ],(req,res)=>{
     // obj={                                // sending json as response (output)
     //     a:'this',               
     //     number:34
     // }
     // res.json(obj);
+    const errors=validationResult(req);
+    if(!errors.isEmpty())
+    {
+        return res.status(400).json({errors:errors.array()});
+    }
 
-    console.log(req.body);
-    res.send("hello");
+    User.create({
+        name:req.body.name,
+        email:req.body.email,
+        password:req.body.password
+        }).then(user=>res.json(user)).catch(err=>{console.log(err) 
+        res.json({"error":"please enter a unique value", "message":err.message})});
+        console.log(req.body);
+    // res.send("hello");
 
-    const user=User(req.body);
-    user.save();
+    // const user=User(req.body);
+    // user.save();
     
 
 })
