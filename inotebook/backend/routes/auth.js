9f29d235a2869307d519073d2d18c3ced1edaab4
ShivamAgrawal150199 <shivam.agrawal1501@gmail.com>
2023-12-18 21:11:26 +0530

    added hashing , salt to store password in db and sending jwttoken to user after authentication. video 49 completed

--- inotebook/backend/routes/auth.js
+++ inotebook/backend/routes/auth.js
@@ -2,8 +2,12 @@ const express=require('express');
 const User = require('../models/User');
 const router= express.Router();
 const { query, validationResult, body } = require('express-validator');
+const bcrypt = require('bcryptjs');        // bcrypt for passowrd hashing and salt pepper
+var jwt = require('jsonwebtoken');            // generating token to be send to client on authorization
 
 // create a user using post /api/auth/createuser. No login required
+
+const JWT_SECRET='iamshivamagrawal$';
 router.post('/createuser',[
     body('email','Enter a valid email').isEmail(),
     body('name','Enter a valid name').isLength({min:3}),
@@ -29,17 +33,29 @@ router.post('/createuser',[
     if (user){
         return res.status(400).json({"error":"The user already exists with this email id so cannot add"});
     }
+    const salt=await bcrypt.genSalt(10);
+    const secpassword= await bcrypt.hash(req.body.password,salt);
+
     user= await User.create({
         name:req.body.name,
         email:req.body.email,
-        password:req.body.password
-        })
-        
+        password:secpassword
+        });
+
+        const data={
+            user:{
+                id:user.id
+            }
+        }
+        const authtoken=jwt.sign(data,JWT_SECRET);
+        console.log(authtoken);
         
         // .then(user=>res.json(user))
         // .catch(err=>{console.log(err) 
         // res.json({"error":"please enter a unique value", "message":err.message})});
-        res.json(user);
+        //res.json(user);  // now not sending users data instead sending authtoken
+
+        res.json({authtoken});  // as we are using ES6 so need to write  res.json({"authtoken":authtoken})
         console.log(req.body);
     }
     catch(error){
