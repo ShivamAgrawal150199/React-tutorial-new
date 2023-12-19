d274ee8d742cbaa9d6f762bb763ecc7d8b0b55ed
ShivamAgrawal150199 <shivam.agrawal1501@gmail.com>
2023-12-19 20:31:18 +0530

    created end points for deleting notes- video 54 completed

--- inotebook/backend/routes/notes.js
+++ inotebook/backend/routes/notes.js
@@ -4,7 +4,7 @@ const fetchuser = require("../middleware/fetchuser");
 const Note = require("../models/Note");
 const { query, validationResult, body } = require("express-validator");
 
-// Route 1 : fetch notes using GET /api/notes/fetchallnotes. No login required
+// Route 1 : fetch notes using GET /api/notes/fetchallnotes.  login required
 
 router.get("/fetchallnotes", fetchuser, async (req, res) => {
   // obj={
@@ -15,7 +15,7 @@ router.get("/fetchallnotes", fetchuser, async (req, res) => {
   res.json(notes);
 });
 
-// Route 2 : insert note using POST /api/notes/addnote. No login required
+// Route 2 : insert note using POST /api/notes/addnote.  login required
 
 router.post(
   "/addnote",
@@ -38,7 +38,7 @@ router.post(
         description,
         tag,
         user: req.user.id,
-      })
+      });
 
       const savedNote = await note.save();
       res.json(savedNote);
@@ -49,40 +49,66 @@ router.post(
   }
 );
 
-
-// Route 3 : update an existing note using PUT /api/notes/updatenote. No login required
+// Route 3 : update an existing note using PUT /api/notes/updatenote. login required
 
 router.put(
-    "/updatenote/:id",
-    fetchuser,
-    [
-      body("title", "Enter a valid name").isLength({ min: 3 }),
-      body("description", "Enter a valid description").isLength({ min: 5 }),
-    ],
-    async (req, res) => {
+  "/updatenote/:id",
+  fetchuser,
+  [
+    body("title", "Enter a valid name").isLength({ min: 3 }),
+    body("description", "Enter a valid description").isLength({ min: 5 }),
+  ],
+  async (req, res) => {
+    try {
+      const { title, description, tag } = req.body;
+      const newnote = {};
+      if (title) newnote.title = title;
+      if (description) newnote.description = description;
+      if (tag) newnote.tag = tag;
 
-        const {title, description,tag}=req.body;
-        const newnote={};
-        if(title) newnote.title=title;
-        if(description) newnote.description=description;
-        if(tag) newnote.tag=tag;
-        
-        let note=await Note.findById(req.params.id);
-        if(!note)
-        {
-            return res.status(404).send("not found");
-        }
+      let note = await Note.findById(req.params.id);
+      if (!note) {
+        return res.status(404).send("not found");
+      }
 
-        if(note.user.toString()!== req.user.id)
-        {
-            return res.status(401).send("not allowed");
-        }
+      if (note.user.toString() !== req.user.id) {
+        return res.status(401).send("not allowed");
+      }
 
-        note=await Note.findByIdAndUpdate(req.params.id , { $set : newnote} ,{new:true} );
+      note = await Note.findByIdAndUpdate(
+        req.params.id,
+        { $set: newnote },
+        { new: true }
+      );
 
-        return res.json(note);
+      return res.json(note);
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).json({ error: "some error occured" });
     }
-)
+  }
+);
 
+// Route 4 : delete an existing note using DELETE /api/notes/deletenote.  login required
+
+router.delete("/deletenote/:id", fetchuser, async (req, res) => {
+  try {
+    let note = await Note.findById(req.params.id);
+    if (!note) {
+      return res.status(404).send("not found");
+    }
+
+    if (note.user.toString() !== req.user.id) {
+      return res.status(401).send("not allowed");
+    }
+
+    note = await Note.findOneAndDelete(req.params.id);
+
+    return res.json({ success: "Deleted successfully", note: note });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ error: "some error occured" });
+  }
+});
 
 module.exports = router;
